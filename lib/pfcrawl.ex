eb52a1cafc436cdbd340ca646137f9b07cad8009
Nicholas Collins <npcollins560@gmail.com>
2023-09-03 16:28:01 -0400

    Initialize Project

---
+++ lib/pfcrawl.ex
@@ -0,0 +1,18 @@
+defmodule Pfcrawl do
+  @moduledoc """
+  Documentation for `Pfcrawl`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pfcrawl.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pfcrawl_test.exs
@@ -0,0 +1,8 @@
+defmodule PfcrawlTest do
+  use ExUnit.Case
+  doctest Pfcrawl
+
+  test "greets the world" do
+    assert Pfcrawl.hello() == :world
+  end
+end
